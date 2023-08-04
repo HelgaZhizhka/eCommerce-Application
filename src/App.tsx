22e7925f51cebc4518aaf013efdacff8a5c23511
wellder00 <wellder00@gmail.com>
2023-08-04 21:10:36 +0300

    refactor: change structure and add axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
+import { API_AUTH_URL, API_SCOPE, API_URL, CLIENT_ID, CLIENT_SECRET, PROJECT_KEY } from './utils/constans';
+
 function App(): JSX.Element {
+  console.log(PROJECT_KEY, CLIENT_ID, CLIENT_SECRET, API_URL, API_AUTH_URL, API_SCOPE);
   return <div className="App">Init</div>;
 }
 
---
+++ src/utils/constans/index.ts
@@ -0,0 +1,6 @@
+export const PROJECT_KEY = process.env.REACT_APP_PROJECT_KEY;
+export const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+export const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
+export const API_URL = process.env.REACT_APP_API_URL;
+export const API_AUTH_URL = process.env.REACT_APP_AUTH_URL;
+export const API_SCOPE = process.env.REACT_APP_SCOPES;
