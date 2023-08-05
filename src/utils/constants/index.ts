c8eca0071a866e6beb864ec3c36521dadfc6aee0
HelgaZhizhka <helgazhyzhka@gmail.com>
2023-08-05 12:13:16 +0200

    refactor: fix constants

---
+++ src/utils/constants/index.ts
@@ -0,0 +1,13 @@
+export const PROJECT_KEY = process.env.REACT_APP_PROJECT_KEY;
+export const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+export const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
+export const API_URL = process.env.REACT_APP_API_URL;
+export const API_AUTH_URL = process.env.REACT_APP_AUTH_URL;
+export const API_SCOPE = process.env.REACT_APP_SCOPES;
+
+export const PROJECT_KEY_CLIENT = process.env.REACT_APP_PROJECT_KEY_CLIENT;
+export const CLIENT_ID_CLIENT = process.env.REACT_APP_CLIENT_ID_CLIENT;
+export const CLIENT_SECRET_CLIENT = process.env.REACT_APP_CLIENT_SECRET_CLIENT;
+export const API_URL_CLIENT = process.env.REACT_APP_API_URL_CLIENT;
+export const API_AUTH_URL_CLIENT = process.env.REACT_APP_AUTH_URL_CLIENT;
+export const API_SCOPE_CLIENT = process.env.REACT_APP_SCOPES_CLIENT;
