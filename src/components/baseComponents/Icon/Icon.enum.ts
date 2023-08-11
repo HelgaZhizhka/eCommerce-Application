a48de44e78a9645184dd98602600143dd8f21e83
HelgaZhizhka <helgazhyzhka@gmail.com>
2023-08-11 19:44:44 +0200

    refactor: change name files for components

---
+++ src/components/baseComponents/Icon/Icon.enum.ts
@@ -0,0 +1,12 @@
+export enum IconName {
+  CART = 'cart',
+  PHONE = 'phone',
+  USER = 'user',
+  UNVISIBLE = 'unvisible',
+  VISIBLE = 'visible',
+  SEARCH = 'search',
+  MAIL = 'mail',
+  WHATSUP = 'whatsup',
+  EXIT = 'exit',
+  FAVORITE = 'favorite',
+}
