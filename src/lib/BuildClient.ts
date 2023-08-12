cca1e2a3c1ea055cedb9bad833d6df82396e8133
VictoriaAlekseeva <vicalekseeva88@gmail.com>
2023-08-12 19:16:53 +0200

    fix: customer login with password flow with sdk

--- src/lib/BuildClient.ts
+++ src/lib/BuildClient.ts
@@ -1,53 +1,45 @@
-// import fetch from 'node-fetch';
 import {
   ClientBuilder,
-  // Import middlewares
   type PasswordAuthMiddlewareOptions,
-  type AuthMiddlewareOptions, // Required for auth
-  type HttpMiddlewareOptions, // Required for sending HTTP requests
+  type HttpMiddlewareOptions,
 } from '@commercetools/sdk-client-v2';
+import {
+  createApiBuilderFromCtpClient,
+} from '@commercetools/platform-sdk';
+import {
+  ByProjectKeyRequestBuilder
+} from '@commercetools/platform-sdk/dist/declarations/src/generated/client/by-project-key-request-builder'
 import envConfig from '../constants/index';
-
 
 const projectKey = `${envConfig.PROJECT_KEY_CLIENT}`;
 const scopes = [`${envConfig.API_SCOPE_CLIENT}`];
 
-// Configure authMiddlewareOptions
-const authMiddlewareOptions: AuthMiddlewareOptions = {
-  host: `${envConfig.API_AUTH_URL_CLIENT}`,
-  projectKey,
-  credentials: {
-    clientId: `${envConfig.CLIENT_ID_CLIENT}`,
-    clientSecret: `${envConfig.CLIENT_SECRET_CLIENT}`,
-  },
-  scopes,
-  fetch,
-};
-
-const passwordAuthMiddlewareOptions: PasswordAuthMiddlewareOptions = {
-  host: `${envConfig.API_AUTH_URL_CLIENT}`,
-  projectKey,
-  credentials: {
-    clientId: `${envConfig.CLIENT_ID_CLIENT}`,
-    clientSecret: `${envConfig.CLIENT_SECRET_CLIENT}`,
-    user: {
-      username: '',
-      password: ''
-    }
-  }
-}
-
-// Configure httpMiddlewareOptions
 const httpMiddlewareOptions: HttpMiddlewareOptions = {
   host: `${envConfig.API_URL_CLIENT}`,
   fetch,
 };
 
-export const ctpClient = new ClientBuilder()
-  .withProjectKey(projectKey) // .withProjectKey() is not required if the projectKey is included in authMiddlewareOptions
-  .withClientCredentialsFlow(authMiddlewareOptions)
-  .withHttpMiddleware(httpMiddlewareOptions)
-  // .withPasswordFlow(passwordAuthMiddlewareOptions)
-  // ...
-  .withLoggerMiddleware() // Include middleware for logging
-  .build();
+export function apiWithPasswordFlow(email:string, password:string):ByProjectKeyRequestBuilder {
+  const passwordAuthMiddlewareOptions: PasswordAuthMiddlewareOptions = {
+    host: `${envConfig.API_AUTH_URL_CLIENT}`,
+    projectKey,
+    credentials: {
+      clientId: `${envConfig.CLIENT_ID_CLIENT}`,
+      clientSecret: `${envConfig.CLIENT_SECRET_CLIENT}`,
+      user: {
+        username: email,
+        password
+      }
+    },
+    scopes,
+    fetch,
+  }
+
+  const ctpClientPassword = new ClientBuilder()
+    .withHttpMiddleware(httpMiddlewareOptions)
+    .withPasswordFlow(passwordAuthMiddlewareOptions)
+    .build();
+
+  const apiRoot = createApiBuilderFromCtpClient(ctpClientPassword).withProjectKey({projectKey});
+  return apiRoot;
+}
--- src/lib/customerLogin.ts
+++ src/lib/customerLogin.ts
@@ -1,5 +1,4 @@
-import {apiRoot} from './Client'
-import envConfig from '../constants/index';
+import {apiWithPasswordFlow} from './BuildClient';
 
 interface LoginResponse {
   status?: number;
@@ -7,31 +6,7 @@ interface LoginResponse {
   error?: Error;
 }
 
-const clientSecret = envConfig.CLIENT_SECRET_CLIENT;
-const clientID = envConfig.CLIENT_ID_CLIENT;
-const encodedCredentials = btoa(`${clientID}:${clientSecret}`);
-const authHeader = `Basic ${encodedCredentials}`;
-
-// const customerLogin = (email: string, password: string): Promise<LoginResponse> => apiRoot
-//   .login()
-//   .post(
-//     {
-//       body: {
-//         email,
-//         password,
-//       },
-//     })
-//   .execute()
-//   .then((response) => {
-//     const status = response.statusCode;
-//     const responseData = response.body;
-//     return { status, data: responseData};
-//   })
-//   .catch((err: Error) => {
-//     throw err;
-//   });
-
-const customerLogin = (email: string, password: string) => apiRoot
+const customerLogin = (email: string, password: string):Promise<LoginResponse> => apiWithPasswordFlow(email, password)
   .me()
   .login()
   .post(
@@ -46,45 +21,20 @@ const customerLogin = (email: string, password: string) => apiRoot
   .then((response) => {
         const status = response.statusCode;
         const responseData = response.body;
-        console.log(responseData)
         return { status, data: responseData};
       })
-
-
-const getCustomerToken = async (email: string, password: string): Promise<JSON> => {
-  const endpoint = `https://auth.europe-west1.gcp.commercetools.com/oauth/ecommerce-project-final-task/customers/token?grant_type=password&username=${email}&password=${password}`;
-  const response = await fetch(endpoint, {
-    method: 'POST',
-    headers: {
-      'Authorization': `${authHeader}`,
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-  });
-
-  if (!response.ok) {
-    throw new Error('Failed to get customer token');
-  }
-  const data = await response.json();
-  return data;
-};
 
 const loginStatus = async (email: string, password: string):Promise<void> => {
   const response = await customerLogin(email, password);
   console.log(response);
 
   if (response.status === 200) {
-    await getCustomerToken(email, password)
-
-    await apiRoot.me().get().execute()
-    console.log(apiRoot)
     alert('Login successful');
   } else if (response.status === 400) {
     alert('statusCode": 400, Customer account with the given credentials not found.');
   } else {
-    console.log(response.status)
     alert(`Unexpected response status: ${response.status}`);
   }
 }
 
 export default loginStatus
-
