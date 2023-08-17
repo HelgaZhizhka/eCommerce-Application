454b7739cf097b23dc61c8d600056908dd78c61c
HelgaZhizhka <helgazhyzhka@gmail.com>
2023-08-17 11:49:39 +0200

    fix: refactor animation page routing with Framer Motion

--- src/components/NavBarMobile/NavBarMobile.tsx
+++ src/components/NavBarMobile/NavBarMobile.tsx
@@ -1,4 +1,4 @@
-import classNames from 'classnames';
+import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
 import CloseIcon from '@mui/icons-material/Close';
 import IconButton from '@mui/material/IconButton';
@@ -15,31 +15,55 @@ type Props = {
   isOpen: boolean;
 };
 
-const NavBarMobile: React.FC<Props> = ({ onClose, isOpen }) => {
-  const navClass = classNames(styles.root, {
-    [styles.open]: isOpen,
-  });
-
-  return (
-    <nav className={(styles.root, navClass)}>
-      <IconButton
-        className={classNames(styles.close)}
-        sx={{ position: 'absolute' }}
-        size="large"
-        color="inherit"
-        aria-label="close"
-        onClick={onClose}
-      >
-        <CloseIcon />
-      </IconButton>
-      <MenuCategories size={'l'} theme={'dark'} />
-      <Link className={classNames(styles.link)} to={RoutePaths.ABOUT}>
-        About Us
-      </Link>
-      <PhoneNumber className={styles.phone}>(+380) 68 018 45 67</PhoneNumber>
-      <InfoPanel className={styles.info} variant={'vertical'} />
-    </nav>
-  );
+const variants = {
+  initial: {
+    x: '-100%',
+    opacity: 0,
+  },
+  open: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      type: 'spring',
+      stiffness: 250,
+      damping: 20,
+    },
+  },
+  closed: {
+    x: '-100%',
+    opacity: 0,
+    transition: {
+      duration: 0.5,
+      ease: 'easeInOut',
+    },
+  },
 };
+
+const NavBarMobile: React.FC<Props> = ({ onClose, isOpen }) => (
+  <motion.nav
+    className={styles.root}
+    initial="initial"
+    animate={isOpen ? 'open' : 'closed'}
+    variants={variants}
+    transition={{ duration: 0.5 }}
+  >
+    <IconButton
+      className={styles.close}
+      sx={{ position: 'absolute' }}
+      size="large"
+      color="inherit"
+      aria-label="close"
+      onClick={onClose}
+    >
+      <CloseIcon />
+    </IconButton>
+    <MenuCategories size={'l'} theme={'dark'} />
+    <Link className={styles.link} to={RoutePaths.ABOUT}>
+      About Us
+    </Link>
+    <PhoneNumber className={styles.phone}>(+380) 68 018 45 67</PhoneNumber>
+    <InfoPanel className={styles.info} variant={'vertical'} />
+  </motion.nav>
+);
 
 export default NavBarMobile;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { observer } from 'mobx-react-lite/dist/index';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate, useLocation } from 'react-router-dom';
+import { motion, AnimatePresence } from 'framer-motion';
 
 import Main from '../pages/Main/Main';
 import ErrorPage from '../pages/ErrorPage/ErrorPage';
@@ -9,19 +10,61 @@ import Registration from '../pages/Registration/Registration';
 import { userStore } from '../stores';
 import { RoutePaths } from './routes.enum';
 
+const pageTransition = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1, transition: { duration: 0.65, ease: 'easeIn' } },
+  exit: { opacity: 0, transition: { duration: 0 } },
+};
+
 const RouterConfig: React.FC = () => {
   const { loggedIn } = userStore;
+  const location = useLocation();
 
   return (
-    <Routes>
-      <Route path={RoutePaths.MAIN} element={<Main />} />
-      <Route path={RoutePaths.LOGIN} element={loggedIn ? <Navigate to={RoutePaths.MAIN} replace /> : <Login />} />
-      <Route
-        path={RoutePaths.REGISTRATION}
-        element={loggedIn ? <Navigate to={RoutePaths.MAIN} replace /> : <Registration />}
-      />
-      <Route path={RoutePaths.ERROR} element={<ErrorPage />} />
-    </Routes>
+    <AnimatePresence>
+      <Routes key={location.pathname}>
+        <Route
+          path={RoutePaths.MAIN}
+          element={
+            <motion.div initial="hidden" animate="visible" exit="exit" variants={pageTransition}>
+              <Main />
+            </motion.div>
+          }
+        />
+        <Route
+          path={RoutePaths.LOGIN}
+          element={
+            loggedIn ? (
+              <Navigate to={RoutePaths.MAIN} replace />
+            ) : (
+              <motion.div initial="hidden" animate="visible" exit="exit" variants={pageTransition}>
+                <Login />
+              </motion.div>
+            )
+          }
+        />
+        <Route
+          path={RoutePaths.REGISTRATION}
+          element={
+            loggedIn ? (
+              <Navigate to={RoutePaths.MAIN} replace />
+            ) : (
+              <motion.div initial="hidden" animate="visible" exit="exit" variants={pageTransition}>
+                <Registration />
+              </motion.div>
+            )
+          }
+        />
+        <Route
+          path={RoutePaths.ERROR}
+          element={
+            <motion.div initial="hidden" animate="visible" exit="exit" variants={pageTransition}>
+              <ErrorPage />
+            </motion.div>
+          }
+        />
+      </Routes>
+    </AnimatePresence>
   );
 };
 
