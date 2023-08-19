e34922be842e5df2c1d975436bead3b03c08d034
HelgaZhizhka <helgazhyzhka@gmail.com>
2023-08-19 19:35:05 +0200

    feat: refactor build to netlify

--- src/index.tsx
+++ src/index.tsx
@@ -4,18 +4,14 @@ import ReactDOM from 'react-dom/client';
 
 import { SvgSprite } from './components/baseComponents/SvgSprite';
 import './index.scss';
-import { RoutePaths } from './routes/routes.enum';
 import App from './App';
-
-const isProduction = process.env.NODE_ENV === 'production';
-const baseName = isProduction ? process.env.REACT_APP_DEPLOY_HOMEPAGE : RoutePaths.MAIN;
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <>
       <SvgSprite />
-      <BrowserRouter basename={baseName}>
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </>
