66df424ea2284358ccc7578b7faa7305b0689c3b
wellder00 <wellder00@gmail.com>
2023-08-22 23:38:50 +0300

    feat: add chip panel

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,8 +1,14 @@
+import { FilterChip } from '../baseComponents/FilterChip';
 import { FilterNestedList } from '../baseComponents/FilterNestedList';
 
 const Filter: React.FC = () => {
   console.log('1');
-  return <FilterNestedList />;
+  return (
+    <div>
+      <FilterNestedList />
+      <FilterChip />
+    </div>
+  );
 };
 
 export default Filter;
---
+++ src/components/baseComponents/FilterChip/FilterChip.tsx
@@ -0,0 +1,64 @@
+import * as React from 'react';
+import Chip from '@mui/material/Chip';
+import Box from '@mui/material/Box';
+import { styled } from '@mui/material/styles';
+
+const CustomChip = styled(Chip)({
+  borderRadius: '16px',
+  padding: '10px',
+  fontSize: '14px',
+  fontWeight: 600,
+  lineHeight: '1.2',
+});
+
+const FilterChip: React.FC = () => {
+  const [activeChips, setActiveChips] = React.useState<string[]>([]);
+
+  const handleChipClick = (label: string): void => {
+    if (activeChips.includes(label)) {
+      setActiveChips(activeChips.filter((chip) => chip !== label));
+    } else {
+      setActiveChips([...activeChips, label]);
+    }
+  };
+
+  return (
+    <>
+      <h3>Size</h3>
+      <Box sx={{ display: 'flex', gap: 1, alignItems: 'center' }}>
+        <CustomChip
+          label="XS"
+          color="primary"
+          variant={activeChips.includes('XS') ? 'filled' : 'outlined'}
+          onClick={(): void => handleChipClick('XS')}
+        />
+        <CustomChip
+          label="S"
+          color="primary"
+          variant={activeChips.includes('S') ? 'filled' : 'outlined'}
+          onClick={(): void => handleChipClick('S')}
+        />
+        <CustomChip
+          label="M"
+          color="primary"
+          variant={activeChips.includes('M') ? 'filled' : 'outlined'}
+          onClick={(): void => handleChipClick('M')}
+        />
+        <CustomChip
+          label="L"
+          color="primary"
+          variant={activeChips.includes('L') ? 'filled' : 'outlined'}
+          onClick={(): void => handleChipClick('L')}
+        />
+        <CustomChip
+          label="XL"
+          color="primary"
+          variant={activeChips.includes('XL') ? 'filled' : 'outlined'}
+          onClick={(): void => handleChipClick('XL')}
+        />
+      </Box>
+    </>
+  );
+};
+
+export default FilterChip;
