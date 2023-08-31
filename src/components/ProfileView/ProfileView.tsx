8e5b9f0dcfac001b084d701ed27007c6d51c9e61
wellder00 <wellder00@gmail.com>
2023-08-31 15:16:30 +0300

    feat: add saveUserData method

--- src/components/ProfileView/ProfileView.tsx
+++ src/components/ProfileView/ProfileView.tsx
@@ -1,15 +1,18 @@
 import Button from '@mui/material/Button';
+import { Customer } from '@commercetools/platform-sdk';
 
 import styles from './ProfileView.module.scss';
 
 type Props = {
   className?: string;
   onModeChange: (mode: boolean) => void;
+  userProfile: Customer | null;
 };
 
-const ProfileView: React.FC<Props> = ({ onModeChange }) => (
+const ProfileView: React.FC<Props> = ({ onModeChange, userProfile }) => (
   <div className={styles.root}>
     <h3>User view</h3>
+    <div>{userProfile?.email}</div>
     <Button variant="outlined" color="primary" onClick={(): void => onModeChange(true)}>
       Edit
     </Button>
