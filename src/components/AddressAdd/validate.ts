279d8776302471d67084a8e9bcfbff4151244290
wellder00 <wellder00@gmail.com>
2023-09-04 17:45:05 +0300

    fix: add search for enter and change validate for postal code

--- src/components/AddressAdd/validate.ts
+++ src/components/AddressAdd/validate.ts
@@ -5,14 +5,8 @@ export const validationSchema = yup.object({
   street: yup.string().required('Street is required'),
   city: yup.string().required('City is required'),
   postalCode: yup
-    .number()
+    .string()
     .required('Postal Code is required')
-    .test('is-five-digits', 'Postal Code must be exactly 5 digits', (value: number) => {
-      if (value) {
-        const postalCode = value.toString();
-        return postalCode.length === 5;
-      }
-      return true;
-    }),
+    .matches(/^\d{5}$/, 'Postal Code must be exactly 5 digits'),
     country: yup.string().required('Country is required'),
 });
