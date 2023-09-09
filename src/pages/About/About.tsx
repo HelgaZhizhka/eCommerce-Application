020110dfc73cc745b290ad6d93b59f5d9b1c38ac
wellder00 <wellder00@gmail.com>
2023-09-09 17:53:27 +0300

    feat: complete about us page

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,14 +1,21 @@
 import Container from '@mui/material/Container';
+import { Box } from '@mui/material';
 import { AboutPerson } from '../../components/AboutPerson';
+
+import logoRs from './image/rs_school_js 1.png';
 
 const About: React.FC = () => (
   <Container
     sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}
     maxWidth="xl"
   >
+    <Box sx={{ mt: 3 }}>
+      <a href="https://rs.school/index.html" target="_blank" rel="noopener noreferrer">
+        <img src={logoRs} alt="Logo RS School" />
+      </a>
+    </Box>
+
     <h2>About us</h2>
-    <AboutPerson />
-    <AboutPerson reverse />
     <AboutPerson />
   </Container>
 );
